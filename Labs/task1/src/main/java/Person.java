ec5cdf7411c6b0d7cb4f74d12ec55e1778c6218b
KazakovKirill <robot5002@mail.ru>
2017-11-25 00:19:30 +0300

    With module task1

--- Labs/task1/src/main/java/Person.java
+++ Labs/task1/src/main/java/Person.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import org.joda.time.LocalDate;
 
 public class Person {
+    private int id;
+    private String fio;
+    private LocalDate birthday;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getAge() {
+        LocalDate today = LocalDate.now();
+        int age = today.getYear() - birthday.getYear();
+        if (today.getDayOfYear() < birthday.getDayOfYear()) age--;
+        return age;
+    }
+
+    public Person(int id, String fio, LocalDate birthday) {
+        setId(id);
+        setFio(fio);
+        this.birthday = birthday;
+    }
 }

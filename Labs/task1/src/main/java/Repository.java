010768cbb795e66f1e8bf9f4e5d7f27dcf5349af
KazakovKirill <robot5002@mail.ru>
2017-11-25 22:41:32 +0300

    delete method

--- Labs/task1/src/main/java/Repository.java
+++ Labs/task1/src/main/java/Repository.java
@@ -27,10 +27,12 @@ public class Repository {
     }
 
     public void delete(int index) {
-        for (int i = index; i < count - 1; i++)
+        for (int i = index; i < count - 1; i++) {
             persons[i] = persons[i + 1];
-        persons[count--] = null;
-        if (count << 1 == persons.length) {
+            persons[i + 1] = null;
+        }
+        //persons[--count] = null;
+        if (--count << 1 == persons.length) {
             persons = Arrays.copyOf(persons, count >> 1);
         }
     }

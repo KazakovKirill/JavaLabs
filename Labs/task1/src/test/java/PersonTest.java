f5a9f18c2b91b719e81ff451853c474043219ee2
KazakovKirill <robot5002@mail.ru>
2017-11-25 15:14:03 +0300

    JUnit tests

---
+++ Labs/task1/src/test/java/PersonTest.java
@@ -0,0 +1,21 @@
+import org.joda.time.LocalDate;
+import org.junit.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest {
+    private Person person;
+
+    @Before
+    public void init() { person = new Person(1, "Казаков Александр Владимирович", LocalDate.parse("1972-03-02")); }
+    @After
+    public void tearDown() { person = null; }
+
+    @Test
+    public void testid() {
+        person.setId(2);
+        assertEquals(2, person.getId());
+        person.setFio("Казаков Кирилл Александрович");
+        assertEquals("Казаков Кирилл Александрович", person.getFio());
+        assertEquals(45, person.getAge());
+    }
+}
---
+++ Labs/task1/src/test/java/RepositoryTest.java
@@ -0,0 +1,57 @@
+import org.joda.time.LocalDate;
+import org.junit.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RepositoryTest {
+    private Repository repository;
+    private Person one, two, three, four, five, six, seven, eight, nine;
+
+    @Before
+    public void init() {
+        repository = new Repository();
+        one = new Person(1, "Буханов Василий Владимирович", LocalDate.parse("1997-05-06"));
+        repository.add(one);
+        two =  new Person(2, "Иванов Андрей Владимирович", LocalDate.parse("1982-08-05"));
+        repository.add(two);
+        three = new Person(3, "Попова Наталия Юрьевна", LocalDate.parse("1995-04-07"));
+        repository.add(three);
+        four = new Person(4, "Кукушкин Сергей Евгеньевич", LocalDate.parse("1972-07-19"));
+        repository.add(four);
+        five = new Person(5, "Власов Сергей Васильевич", LocalDate.parse("1966-09-27"));
+        repository.add(five);
+        six = new Person(6, "Теплинских Елена Сергеевна", LocalDate.parse("1979-03-01"));
+        repository.add(six);
+        seven = new Person(7, "Кротов Алексей Юрьевич", LocalDate.parse("1984-10-06"));
+        repository.add(seven);
+        eight = new Person(8, "Кайков Андрей Альбертович", LocalDate.parse("1971-12-25"));
+        repository.add(eight);
+        nine = new Person(9, "Конусов Павел Иванович", LocalDate.parse("1975-04-10"));
+        repository.add(nine);
+    }
+
+    @After
+    public void tearDown() {
+        repository = null;
+    }
+
+    @Test
+    public void testBubbleSort() {
+        repository.bubbleSort((a, b) -> a.getFio().length() - b.getFio().length());
+        Person[] persons = new Person[] { three, seven, nine, five, eight, four, two, six, one };
+        assertArrayEquals(persons, repository.getPersons());
+    }
+
+    @Test
+    public void testShakerSort() {
+        repository.shakerSort((a, b) -> b.getAge() - a.getAge());
+        Person[] persons = new Person[] { five, four, eight, nine, six, two, seven, three, one};
+        assertArrayEquals(persons, repository.getPersons());
+    }
+
+    @Test
+    public void testQuickSort() {
+        repository.quickSort((a, b) -> Math.abs(a.getAge() - 30) - Math.abs(b.getAge() - 30));
+        for (Person p : repository.getPersons())
+            System.out.println(p.toString());
+    }
+}
